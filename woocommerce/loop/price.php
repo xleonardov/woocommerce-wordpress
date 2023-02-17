188157f389c084041833dbbb07cabca49b562655
LeonV <leonardov0529@gmail.com>
2023-02-17 16:00:41 +0000

    more small fixes

--- woocommerce/loop/price.php
+++ woocommerce/loop/price.php
@@ -10,37 +10,37 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see         https://docs.woocommerce.com/document/template-structure/
- * @package     WooCommerce\Templates
- * @version     1.6.4
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 1.6.4
  */
 
 if (!defined('ABSPATH')) {
-	exit; // Exit if accessed directly
+    exit; // Exit if accessed directly
 }
 
 global $product;
 $percentage = 0;
 if ($product->is_type('simple')) { //if simple product
-	if ($product->sale_price) {
-		$percentage = round(((floatval($product->regular_price) - floatval($product->sale_price)) / floatval($product->regular_price)) * 100);
-	}
+    if ($product->sale_price) {
+        $percentage = round(((floatval($product->regular_price) - floatval($product->sale_price)) / floatval($product->regular_price)) * 100);
+    }
 } else { //if variable product
-	$percentage = apply_filters('get_variable_sale_percentage', $product);
+    $percentage = apply_filters('get_variable_sale_percentage', $product);
 }
 ?>
 
-<?php if ($price_html = $product->get_price_html()): ?>
-	<div class="flex justify-center items-center space-x-2">
-		<span class="loop-price text-base uppercase text-center">
-			<?php echo $price_html; ?>
-		</span>
-		<?php if ($percentage && $percentage > 0) { ?>
-			<div>
-				<span class="bg-amarelo text-white rounded-sm text-sm px-1 py-px sm:px-2 sm:py-1 font-roboto">
-					<?='-' . $percentage ?>%
-				</span>
-			</div>
-		<?php } ?>
-	</div>
+<?php if ($price_html = $product->get_price_html()) : ?>
+    <div class="flex justify-center items-center space-x-2">
+        <span class="loop-price text-base uppercase text-center">
+    <?php echo $price_html; ?>
+        </span>
+    <?php if ($percentage && $percentage > 0) { ?>
+            <div>
+                <span class="bg-amarelo text-white rounded-sm text-sm px-1 py-px sm:px-2 sm:py-1 font-roboto">
+        <?php echo '-' . $percentage ?>%
+                </span>
+            </div>
+    <?php } ?>
+    </div>
 <?php endif; ?>
