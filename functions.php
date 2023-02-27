8227139778e5644d5d1a3c2ca9a302b7df48cef7
LeonV <leonardov0529@gmail.com>
2023-02-27 16:02:19 +0000

    fix rest access

--- functions.php
+++ functions.php
@@ -1046,3 +1046,26 @@ add_action('wp_head', function () {
     </script>
 <?php
 }, 10, 0);
+
+add_filter( 'rest_authentication_errors', function( $result ) {
+        // If a previous authentication check was applied,
+        // pass that result along without modification.
+        if ( true === $result || is_wp_error( $result ) ) {
+            return $result;
+        }
+     
+        // No authentication has been performed yet.
+        // Return an error if user is not logged in.
+        if ( ! is_user_logged_in() ) {
+            return new WP_Error(
+                'rest_not_logged_in',
+                __( 'You are not currently logged in.' ),
+                array( 'status' => 401 )
+            );
+        }
+     
+        // Our custom authentication check should have no effect
+        // on logged-in requests
+        return $result;
+    });
+
